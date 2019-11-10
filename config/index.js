d3b322ad7f68b01bac250fd3d69ca2e9a7c9bd46
Vladimir V. Myagdeev <vvmspace@gmail.com>
2019-11-10 20:25:57 +0500

    Step 1: Empty node.js project with express and body-parser

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
 module.exports = {
-
+    server: {
+        port: process.env.PORT || 7070,
+        host: process.env.HOST || '127.0.0.1',
+    }
 };
---
+++ index.js
@@ -0,0 +1,22 @@
+const config = require('./config');
+const express = require('express');
+const body = require('body-parser');
+const app = express();
+const log = require('./libs/log');
+
+const {port, host} = config.server;
+
+async function start() {
+    app.use(body.urlencoded({ extended: true, limit: "50mb" }));
+    app.use(body.json({ limit: "50mb" }));
+
+    app.get('/', (req, res) => {
+        res.json({text: 'Welcome'});
+    });
+
+    app.listen(port, host, () => {
+        log(`Listening ${host}:${port}`);
+    });
+}
+
+start();
