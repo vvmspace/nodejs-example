bc4aa94a98f7a8bb6f58fd55788fc32d78457b3f
Vladimir V. Myagdeev <vvmspace@gmail.com>
2020-02-19 20:27:34 +0500

    process.env.cron_part

--- cron/index.js
+++ cron/index.js
@@ -3,4 +3,6 @@ const cron = require('node-cron');
 // Import tasks here:
 const parse = require('./parse'); // simple task example
 
-cron.schedule('0 0 0 0 0 0', parse);
+if (process.env.cron_parse) {
+    cron.schedule('0 0 0 * * *', parse);
+}
